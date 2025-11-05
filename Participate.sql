9866bc77132cd275a1fd809be7df5943a5f9f806
byuu-byuu <byuu.student@gmail.com>
2025-11-05 16:24:51 +0630

    Update Participate.sql

--- Participate.sql
+++ Participate.sql
@@ -1,4 +1,15 @@
 
- create table Participate  (Pid int(5) , Car_name varchar(15),Report_no int(10),Damage_amount, int(15));
-insert into Participate values (1,'Toyota Crown', '004', '70000'),('2','Toyata Belta','005','30000'),
-(3,'Suzuki','001','40000'),(4,'Suzuki','002','60000');
+CREATE TABLE Participate (
+    Pid INT(5),
+    Car_name VARCHAR(15),
+    Report_no INT(10),
+    Damage_amount INT(15)
+);
+
+INSERT INTO Participate (Pid, Car_name, Report_no, Damage_amount) VALUES
+(1, 'Toyota Crown', 4, 70000),
+(2, 'Toyota Belta', 5, 30000),
+(3, 'Suzuki', 1, 40000),
+(4, 'Suzuki', 2, 60000);
+
+
