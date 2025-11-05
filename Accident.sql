1106c0130af1cb1ef97d74b19b3ec308df556189
byuu-byuu <byuu.student@gmail.com>
2025-11-05 16:23:01 +0630

    Update Accident.sql

--- Accident.sql
+++ Accident.sql
@@ -1,3 +1,12 @@
-create table Accident (Report_no int(10), Date date , Location varchar(50));
-insert into Accident values('001','2022-10-15','Junction Point'),('002','2022-08-12','Mayangone'),('003','2022-04-13','8-mile point'),
-('004'2023-02-16','Sanchaung'),('005','2023-04-30', 'AungMingalar');
+CREATE TABLE Accident (
+    Report_no INT,
+    Accident_date DATE,
+    Location VARCHAR(50)
+);
+
+INSERT INTO Accident (Report_no, Accident_date, Location) VALUES
+(1, '2022-10-15', 'Junction Point'),
+(2, '2022-08-12', 'Mayangone'),
+(3, '2022-04-13', '8-mile'),
+(4, '2023-02-16', 'Sanchaung'),
+(5, '2023-04-30', 'Aung Mingalar');
